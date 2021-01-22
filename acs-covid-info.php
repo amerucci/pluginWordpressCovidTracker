87e7c6f01208a3833bc83683f37fd69e5f43aeba
amerucci <a.merucci@onlineformapro.com>
2021-01-22 10:11:02 +0100

    Plugin wordpress Covid Tracker

--- acs-covid-info.php
+++ acs-covid-info.php
@@ -10,5 +10,5 @@
  Text Domain: ACS - Covid Tracker 
  */
 
-// Include mfp-functions.php, use require_once to stop the script if mfp-functions.php is not found
+
 require_once plugin_dir_path(__FILE__) . 'includes/config.php';
